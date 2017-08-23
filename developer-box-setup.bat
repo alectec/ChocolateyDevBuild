47449f97f72e9e009182ae6412a89555e406926e
Jamie Hurley <jhurley@alectec.com>
2017-08-22 23:33:48 -0400

    Update developer-box-setup.bat

--- developer-box-setup.bat
+++ developer-box-setup.bat
@@ -1,6 +1,5 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-choco install googlechrome
 choco install netfx-4.6.1-devpack
 choco install google-chrome-x64
 choco install resharper
